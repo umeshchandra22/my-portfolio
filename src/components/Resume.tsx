5e5639f780504f7d95f9b1ab7035160e1084fad6
umesh chandra <67307159+umeshchandra22@users.noreply.github.com>
2025-02-28 16:42:16 +0530

    Update Resume.tsx

--- src/components/Resume.tsx
+++ src/components/Resume.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Document, Page, Text, View, StyleSheet } from '@react-pdf/renderer';
+import { Document, Page, Text, View, StyleSheet, Image } from '@react-pdf/renderer';
 
 // Create styles
 const styles = StyleSheet.create({
